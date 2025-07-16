09940fc7ba1199878c83ca38a969842c2cf036ab
arnoldfromancius <arnoldfromancius@gmail.com>
2025-07-16 14:17:58 +0200

    learning

---
+++ Jscript/functions.js
@@ -0,0 +1,22 @@
+//function declaration
+function reg_func(){
+	console.log("This is a regular function");
+}
+
+//arrow function syntax
+let arrow = () => {
+	console.log("This is an arrow function");
+}
+
+//arrow func are usaually 1 liners, return is omitted
+let arrow_oneliner = () => console.log("This is one line arrow function");
+
+//function expression syntax
+let expr = function(){
+	console.log("This is a function expression");
+}
+
+reg_func();
+arrow();
+arrow_oneliner();
+expr();
