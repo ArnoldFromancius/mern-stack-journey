9a95cf97b0435c0d324d3f4a0e38b614104a53e9
arnoldfromancius <arnoldfromancius@gmail.com>
2025-08-05 13:23:18 +0200

    learning...

--- Jscript/API.js
+++ Jscript/API.js
@@ -1,9 +1,13 @@
-//Working with APIs, what we need to know; -fetch() is used to access an API
+//Working with APIs, what we need to know; *fetch() is used to access an API
 //We'll use the catfacts.ninja website to demonstrate this, the website provides
 //an API which returns a .json of a random cat fact(or list of facts)
 
+
+//THIS SOLUTION UTILIZES PROMISE CHAINING
 //fetch() returns a promise
-fetch('https://catfact.ninja/fact')	//json is returned in raw format
-.then((res) => res.text())	//we convert the json to readable text
-.then(txt => console.log(txt))	//self explainatory
-.catch(() => console.log('Error!'));	//error handling
+//fetch('https://catfact.ninja/fact')	//json is returned in raw format
+//.then((res) => res.text())	//we convert the json to readable text
+//.then(txt => console.log(txt))	//self explainatory
+//.catch(() => console.log('Error!'));	//error handling
+
+//THIS SOLUTION UTILIZES ASYNC-AWAIT
