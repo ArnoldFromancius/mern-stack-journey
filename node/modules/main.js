021102ed5dea291b1db90511b812a78e056dd2a7
arnoldfromancius <arnoldfromancius@gmail.com>
2025-08-08 12:15:32 +0200

    learning...

--- node/modules/main.js
+++ node/modules/main.js
@@ -1,5 +1,5 @@
 //To access items(functions,variables...) in a different file(module) 
 //we first need to import it using the require() function.
 
-let mod = require('./module');
+const mod = require('./module');
 mod.example_module();
