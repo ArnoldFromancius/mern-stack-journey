bb61ac2aca09b95faad72882a089e60fe6678dc5
arnoldfromancius <arnoldfromancius@gmail.com>
2025-08-08 11:58:09 +0200

    learning...

---
+++ node/modules/module.js
@@ -0,0 +1,10 @@
+//This module will be exportable. Basically we have an object 'module' 
+//that has a property 'exports'. We can add the items(methods,variables...)
+//to this property and access them in other modules(files)
+
+let sampleText = "This is a test module";
+
+let example_module = function () => console.log(sampleText);
+
+//now to make the method example_module accessible to other modules
+module.exports.example_module = example_module;
