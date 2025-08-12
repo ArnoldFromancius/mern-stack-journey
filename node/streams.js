ab92e219c6a9d28a57a045c5c74f5eb4ec7ab654
arnoldfromancius <arnoldfromancius@gmail.com>
2025-08-12 11:59:18 +0200

    learning...

---
+++ node/streams.js
@@ -0,0 +1,12 @@
+//streams allow us to use a file before completly downloading/loading it
+//the file is transmitted in chuncks into a buffer...
+
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./files/largesample.txt');
+
+readStream.on('data', (chunck) => {
+	console.log('--- CHUNCK ---');
+	console.log(chunck);
+});
+
