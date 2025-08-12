3685d22380287693d91b5d284fa175549bbdfe1b
arnoldfromancius <arnoldfromancius@gmail.com>
2025-08-12 11:11:21 +0200

    learning...

--- node/filesystem.js
+++ node/filesystem.js
@@ -2,13 +2,20 @@
 
 const fs = require('fs');
 
+//creating a directory
+fs.mkdir('./files', (err) => {
+	if(err){
+		console.log('error creating folder');
+	}
+});
+
 //writing to files
-fs.writeFile('./samplefile.txt', 'Written to file from js...', () => {
+fs.writeFile('./files/samplefile.txt', 'Written to file from js...', () => {
 	console.log("file written");
 });
 
 //reading files
-fs.readFile('./samplefile.txt', (err, data) => {
+fs.readFile('./files/samplefile.txt', (err, data) => {
 	if(err){
 		console.log(err);
 	}
