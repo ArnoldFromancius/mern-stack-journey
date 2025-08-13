e3f820fc2a10d844684c50e985c2323fc5282340
arnoldfromancius <arnoldfromancius@gmail.com>
2025-08-13 10:30:25 +0200

    learning...

--- node/servers/server.js
+++ node/servers/server.js
@@ -1,2 +1,11 @@
 //We create a basic server
 
+let http = require('http');
+
+let server = http.createServer((req, res) => {
+	console.log('request received!');	
+});
+
+server.listen(1000, 'localhost', () => {
+	console.log('listening on port 1000...');
+});
