bd0109b5fafcbd3a05ccdcfee61a4c99b244978c
arnoldfromancius <arnoldfromancius@gmail.com>
2025-08-15 22:07:30 +0200

    learning...

--- node/servers/server.js
+++ node/servers/server.js
@@ -4,7 +4,8 @@ let http = require('http');
 
 let server = http.createServer((req, res) => {
 	console.log('request received!');
-	console.log(req);
+	res.setHeader('Content-Type','text/plain');
+	res.end('Response from server...');
 });
 
 server.listen(1000, 'localhost', () => {
