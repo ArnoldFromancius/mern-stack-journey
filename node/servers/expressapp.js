3af9e9d06fd055109f3b075d66a81c36db88a862
arnoldfromancius <arnoldfromancius@gmail.com>
2025-08-16 00:45:47 +0200

    learning...

--- node/servers/expressapp.js
+++ node/servers/expressapp.js
@@ -4,5 +4,11 @@ let app = express();
 
 app.listen(2000);
 
+app.get('/', (req, res) => {
+	res.sendFile('./views/index.html', { root: __dirname });
+});
 
+app.get('/info', (req,res) => {
+	res.sendFile('./views/info.html', {root: __dirname });
+});
 
