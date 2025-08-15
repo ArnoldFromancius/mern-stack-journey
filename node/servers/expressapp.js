fccb578910961b4a4dd38949a82e9d39c0e86393
arnoldfromancius <arnoldfromancius@gmail.com>
2025-08-16 01:08:56 +0200

    learning...

--- node/servers/expressapp.js
+++ node/servers/expressapp.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 let app = express();
 
-app.listen(2000);
-
 app.get('/', (req, res) => {
 	res.sendFile('./views/index.html', { root: __dirname });
 });
@@ -12,3 +10,8 @@ app.get('/info', (req,res) => {
 	res.sendFile('./views/info.html', {root: __dirname });
 });
 
+app.use((req, res) => {
+	res.sendFile('./views/missing.html', {root: __dirname });
+});
+
+app.listen(2000);
