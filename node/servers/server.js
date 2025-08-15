44f356e028e457d92d4317c83956aa7e9aa56057
arnoldfromancius <arnoldfromancius@gmail.com>
2025-08-15 23:14:11 +0200

    learning...

--- node/servers/server.js
+++ node/servers/server.js
@@ -5,13 +5,26 @@ let fs = require('fs');
 
 let server = http.createServer((req, res) => {
 	console.log('request received!');
-	fs.readFile('./views/index.html', (err, data) => {
+	let path = './views/';
+	switch(req.url){
+		case '/':
+			path += 'index.html';
+			break;
+		case 'info':
+			path += 'info.html';
+			break;
+		default: path += 'missing.html';
+			break;
+	}
+
+	fs.readFile(path, (err, data) => {
 		if(err){
 			console.log(err);
 		}
 		res.setHeader('Content-Type','text/html');
 		res.write(data);
 		res.end();
+		
 	});
 });
 
