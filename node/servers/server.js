72f5073b7375ad3311af48c9b78086ad609f4dbc
arnoldfromancius <arnoldfromancius@gmail.com>
2025-08-15 22:48:03 +0200

    learning...

--- node/servers/server.js
+++ node/servers/server.js
@@ -1,11 +1,18 @@
 //We create a basic server
 
 let http = require('http');
+let fs = require('fs');
 
 let server = http.createServer((req, res) => {
 	console.log('request received!');
-	res.setHeader('Content-Type','text/plain');
-	res.end('Response from server...');
+	fs.readFile('./views/index.html', (err, data) => {
+		if(err){
+			console.log(err);
+		}
+		res.setHeader('Content-Type','text/plain');
+		res.write(data);
+		res.end();
+	});
 });
 
 server.listen(1000, 'localhost', () => {
