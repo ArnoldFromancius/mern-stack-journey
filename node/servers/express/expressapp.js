241e461ca0da0de66878b496f46e03f21e2aaf41
arnoldfromancius <arnoldfromancius@gmail.com>
2025-09-18 12:44:25 +0200

    learning...

--- node/servers/express/expressapp.js
+++ node/servers/express/expressapp.js
@@ -1,53 +1,54 @@
-//rewriting our server using express. 
-
-const express = require('express');
-
-let app = express();
-
-//database
+const express = require("express");
 const mongoose = require("mongoose");
-//connect to remote db
-mongoose.connect("mongodb+srv://arnoldfromancius_db_user:%23siliconwhisperer2799@mighkacarepluscluster.a5owpj6.mongodb.net/?retryWrites=true&w=majority&appName=MighkaCareplusCluster")
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
 
-//create a user schema
-const userSchema = new Schema({
-	name: String,
-	address: String,
-	phone: String,
-	email: String
-})
+const app = express();
 
-//create a user model
-const User = model('User', userSchema);
+// connect to DB
+mongoose
+  .connect(
+    "mongodb+srv://arnoldfromancius_db_user:%23siliconwhisperer2799@mighkacarepluscluster.a5owpj6.mongodb.net/?retryWrites=true&w=majority&appName=MighkaCareplusCluster"
+  )
+  .then(async () => {
+    console.log("MongoDB connected");
 
-//create a user object
-const userobj = new User({
-	name: "Arnold",
-	address: "Ndeke Village",
-	phone: "0977777777",
-	email: "mail@mail.com
+    // create schema + model
+    const userSchema = new mongoose.Schema({
+      name: String,
+      address: String,
+      phone: String,
+      email: String,
+    });
+
+    const User = mongoose.model("User", userSchema);
+
+    // create + save user
+    const userobj = new User({
+      name: "Arnold",
+      address: "Ndeke Village",
+      phone: "0977777777",
+      email: "mail@mail.com",
+    });
+
+    await userobj.save();
+
+    // log first user
+    const firstUser = await User.findOne({});
+    console.log(firstUser);
+  })
+  .catch((err) => console.error(err));
+
+// ROUTING
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
 });
 
-//save user to db
-await userobj.save();
-
-//log the users in db
-const firstUser = await User.findOne({});
-console.log(firstUser);
-
-//ROUTING
-app.get('/', (req, res) => {
-	res.sendFile('./views/index.html', { root: __dirname });
-});
-
-app.get('/info', (req,res) => {
-	res.sendFile('./views/info.html', {root: __dirname });
+app.get("/info", (req, res) => {
+  res.sendFile("./views/info.html", { root: __dirname });
 });
 
 app.use((req, res) => {
-	res.sendFile('./views/missing.html', {root: __dirname });
+  res.sendFile("./views/missing.html", { root: __dirname });
 });
 
-app.listen(2000);
+app.listen(2000, () => console.log("Server running on port 2000"));
+
