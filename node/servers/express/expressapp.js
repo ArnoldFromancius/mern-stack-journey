6950da3f9826eb157774ff5f065ecb3b6c5c2836
arnoldfromancius <arnoldfromancius@gmail.com>
2025-09-18 12:11:37 +0200

    learning...

--- node/servers/express/expressapp.js
+++ node/servers/express/expressapp.js
@@ -4,6 +4,24 @@ const express = require('express');
 
 let app = express();
 
+//dA
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://arnoldfromancius_db_user:%23siliconwhisperer2799@mighkacarepluscluster.a5owpj6.mongodb.net/?retryWrites=true&w=majority&appName=MighkaCareplusCluster")
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
+
+const userSchema = new Schema({
+	name: String,
+	address: String,
+	phone: String,
+	email: String
+})
+
+const User = model('User', userSchema);
+
+
+//ROUTING
 app.get('/', (req, res) => {
 	res.sendFile('./views/index.html', { root: __dirname });
 });
