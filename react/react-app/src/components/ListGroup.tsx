d81289398af5e31cfdb4021945eba6f58a277cab
arnoldfromancius <arnoldfromancius@gmail.com>
2025-09-29 15:52:27 +0200

    learning...

--- react/react-app/src/components/ListGroup.tsx
+++ react/react-app/src/components/ListGroup.tsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 
 function ListGroup() {
     let items = ['fridge', 'washing machine', 'stove', 'geyser', 'Genset', 'sink', 'bathtub'];
-    let selectedIndex = 0;
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+    
     return (
         <ul className="list-group">
             
             {items.map((item, index) => (
-                <li className={selectedIndex === index ? "list-group-item active": "list-group-item"} onClick={() => console.log(item)} key={item}>{item}</li>
+                <li className={selectedIndex === index ? "list-group-item active": "list-group-item"} onClick={() => setSelectedIndex(index)} key={item}>{item}</li>
             ))};
             
         </ul>
