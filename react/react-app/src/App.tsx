2ada89069b8ba93337a8752fa6b15ee28a04d72b
arnoldfromancius <arnoldfromancius@gmail.com>
2025-09-29 11:32:08 +0200

    learning...

--- react/react-app/src/App.tsx
+++ react/react-app/src/App.tsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Message from './Message';
+import ListGroup from './components/ListGroup';
 
 function App() {
 
-  return <Message></Message>;
+  return (
+    <>
+      <Message />;
+      <ListGroup />;
+    </>
+  );
+
 }
 
 export default App;
