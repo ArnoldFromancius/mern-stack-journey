727fe887210941d6762e9c1762e89ade9ce6f843
arnoldfromancius <arnoldfromancius@gmail.com>
2025-09-29 15:10:46 +0200

    learning...

--- react/react-app/src/components/ListGroup.tsx
+++ react/react-app/src/components/ListGroup.tsx
@@ -1,12 +1,12 @@
 
 function ListGroup() {
     let items = ['fridge', 'washing machine', 'stove', 'geyser', 'Genset', 'sink', 'bathtub'];
-
+    let selectedIndex = 0;
     return (
         <ul className="list-group">
             
-            {items.map((item) => (
-                <li className="list-group-item" onClick={() => console.log(item)} key={item}>{item}</li>
+            {items.map((item, index) => (
+                <li className={selectedIndex === index ? "list-group-item active": "list-group-item"} onClick={() => console.log(item)} key={item}>{item}</li>
             ))};
             
         </ul>
